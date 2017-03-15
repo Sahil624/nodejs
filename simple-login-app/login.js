929af54737d08d10e5493c454faaf98f86b28579
Sahil Mangotra <sahilmangotra1997@gmail.com>
2017-03-15 20:42:33 +0530

    Simple Login app made

---
+++ simple-login-app/login.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var sessions = require('express-session')
+var bodyparser = require('body-parser');
+var app = express();
+
+var session;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use(sessions({
+	secret : 'fdf87wye1iehiw2ew8973g',
+	resave : false,
+	saveUninitialized:true
+}))
+
+app.get('/login',function (req,res) {
+	session = req.session;
+	if(session.uni){
+		res.redirect('/redirect')
+	}
+	res.sendFile('./pages/login.html',{root:__dirname});
+});
+
+app.post('/login',function (req,res) {
+
+	//res.end(JSON.stringify(req.body))
+	session = req.session;
+
+	if(session.uni){
+		res.redirect('/redirect')
+	}
+
+	if(req.body.username=='admin'&& req.body.pass=='admin'){
+		session.uni = req.body.username;
+	}
+
+	res.redirect('/redirect');
+
+});
+
+app.get('/logout',function(req,res){
+	req.session.destroy(function(err){
+		if(err){
+			console.log(err);
+			res.redirect('/login');
+		}
+	})
+})
+
+app.get('/redirect',function(req,res){
+	session = req.session;
+
+	if(session.uni){
+		res.redirect('/admin');
+	}
+
+	else{
+		res.end("Who are you?")
+	}
+});
+
+
+
+app.listen(1337);
